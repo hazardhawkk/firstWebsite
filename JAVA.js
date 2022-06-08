20f7f8cc73ec2f2334ddb6562ea6716e97351ad4
hazardhawkk <shahzaibzafarshazi@gmail.com>
2022-06-08 11:51:16 +0100

    ..

--- JAVA.js
+++ JAVA.js
@@ -13,7 +13,7 @@ function hash(str)
 
 function CheckAnswer(actualAns, questionNum) //questionNum is a string, actualAns is hash
 {
-let attemptedAns = hash(document.getElementById("Ans"+questionNum).textContent); //gets hash of answer attempted
+let attemptedAns = hash(document.getElementById("Ans"+questionNum).value); //gets hash of answer attempted
 
 if (attemptedAns == actualAns){response="CORRECT!!";}
 else{response="no...";}
