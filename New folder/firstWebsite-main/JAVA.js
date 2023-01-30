06cedfc3e4ef888d09758c283b3842e69cdcc9a6
hazardhawkk <shahzaibzafarshazu@gmail.com>
2023-01-30 15:45:34 +0000

    test 1

---
+++ New folder/firstWebsite-main/JAVA.js
@@ -0,0 +1,36 @@
+var response = " ";
+
+function hash(str)
+{ 
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      const char = str.charCodeAt(i);
+      hash = (hash << 5) - hash + char;
+      hash &= hash; // Convert to 32bit integer
+    }
+    return new Uint32Array([hash])[0].toString(36);
+}
+
+function CheckAnswer(actualAns, questionNum) //questionNum is a string, actualAns is hash
+{
+let attemptedAns = hash(document.getElementById("Ans"+questionNum).value); //gets hash of answer attempted    //need to research why .valaue works but others do not, like innerHTML,textContent
+
+if (attemptedAns == actualAns){response="CORRECT!!";}
+else{response="no...";}
+
+document.getElementById("response"+questionNum).innerHTML=response;  //<- writes response
+}
+
+var text = "general";
+
+function search()
+{
+  document.getElementById("Search").innerHTML="gfdgfdg";
+let searchText = document.getElementById("Search").value;
+
+let parent = document.getElementById("Models");
+let child = document.getElementById(searchText);
+parent.removeChild(child);
+}
+
+
